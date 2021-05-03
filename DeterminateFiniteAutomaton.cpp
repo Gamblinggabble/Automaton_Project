cd9a0735d1546d8ade8beb3eb0fd0b9b3ca8d78e
Gabble <gabriela.dim_cheva@abv.bg>
2021-05-03 17:38:46 +0300

    Included the State functionality in DeterminateFiniteAutomaton

--- DeterminateFiniteAutomaton.cpp
+++ DeterminateFiniteAutomaton.cpp
@@ -2,110 +2,100 @@
 #include <iostream>
 #include <cstring>
 
-DeterminateFiniteAutomaton<char>::DeterminateFiniteAutomaton(unsigned size, char* alphabet) {
-	if (alphabet != nullptr) {
-		//size is number of elements, not counting '\0'
-		this->size = size;
-		this->alphabet = new char[size + 1];
-		strcpy_s(this->alphabet, size + 1, alphabet);
-	}
-	else {
-		std::cout << "No alphabet entered! Default alphabet set - {a,b}" << std::endl;
-		this->size = 2;
-		this->alphabet = new char[this->size + 1]{ 'a', 'b' };
-	}
-}
-DeterminateFiniteAutomaton<int>::DeterminateFiniteAutomaton(unsigned size, int* alp) {
-	if (alp != nullptr) {
-		this->size = size;
-		this->alphabet = new int[size];
-		for (unsigned i = 0; i < size; i++)
-		{
-			this->alphabet[i] = alp[i];
-		}
-	}
-	else {
-		std::cout << "No alphabet entered! Default alphabet set - {0,1}" << std::endl;
-		this->size = 2;
-		this->alphabet = new int[2]{ 0, 1 };
-	}
-}
-
-
-DeterminateFiniteAutomaton<char>::DeterminateFiniteAutomaton(const DeterminateFiniteAutomaton<char>& rhs) {
-	if (rhs.alphabet != nullptr) {
-		size = rhs.size;
-		alphabet = new char[size + 1];
-		strcpy_s(alphabet, size + 1, rhs.alphabet);
-	}
-	else {
-		size = 0;
-		alphabet = nullptr;
-	}
-}
-DeterminateFiniteAutomaton<int>::DeterminateFiniteAutomaton(const DeterminateFiniteAutomaton<int>& rhs) {
-	if (rhs.alphabet != nullptr) {
-		size = rhs.size;
-		alphabet = new int[size];
-		for (unsigned i = 0; i < size; i++)
-		{
-			alphabet[i] = rhs.alphabet[i];
-		}
-	}
-	else {
-		size = 0;
-		alphabet = nullptr;
-	}
-}
-
-DeterminateFiniteAutomaton<char>::~DeterminateFiniteAutomaton() {
-	if (alphabet != nullptr) {
-		delete[] alphabet;
-	}
-}
-DeterminateFiniteAutomaton<int>::~DeterminateFiniteAutomaton() {
-	if (alphabet != nullptr) {
-		delete[] alphabet;
-	}
-}
-
-DeterminateFiniteAutomaton<char>& DeterminateFiniteAutomaton<char>::operator=(const DeterminateFiniteAutomaton<char>& rhs) {
-	if (this != &rhs) {
-		if (rhs.alphabet != nullptr) {
-			size = rhs.size;
-			alphabet = new char[size + 1];
-			strcpy_s(alphabet, size + 1, rhs.alphabet);
-		}
-	}
-
-	return *this;
-}
-DeterminateFiniteAutomaton<int>& DeterminateFiniteAutomaton<int>::operator=(const DeterminateFiniteAutomaton<int>& rhs) {
-	if (this != &rhs) {
-		if (rhs.alphabet != nullptr) {
-			size = rhs.size;
-			alphabet = new int[size];
-			for (unsigned i = 0; i < size; i++)
-			{
-				alphabet[i] = rhs.alphabet[i];
-			}
-		}
-	}
-
-	return *this;
-}
-
-int DeterminateFiniteAutomaton<char>::printAlphabet() {
-	for (unsigned i = 0; i < size; i++)
-	{
-		std::cout << alphabet[i] << " ";
-	}
-	return 0;
-}
-int DeterminateFiniteAutomaton<int>::printAlphabet() {
-	for (unsigned i = 0; i < size; i++)
-	{
-		std::cout << alphabet[i] << " ";
-	}
-	return 0;
-}
+//DFAutomaton<char>::DFAutomaton(unsigned alphabetSize, char* alphabet, unsigned statesCnt, State* states, const State& entryState,
+//	unsigned finalStatesCnt, State* finalStates)
+//	:statesCnt(statesCnt), states(new State[statesCnt]), entryState(entryState), finalStatesCnt(finalStatesCnt),
+//	finalStates(new State[finalStatesCnt])
+//{
+//	if (alphabet != nullptr) {
+//		//size is number of elements, not counting '\0'
+//		this->alphabetSize = alphabetSize;
+//		this->alphabet = new char[alphabetSize + 1];
+//		strcpy_s(this->alphabet, alphabetSize + 1, alphabet);
+//	}
+//	else {
+//		std::cout << "No alphabet entered! Default alphabet set - {a,b}" << std::endl;
+//		this->alphabetSize = 2;
+//		this->alphabet = new char[this->alphabetSize + 1]{ 'a', 'b' };
+//	}
+//
+//	if (states != nullptr) {
+//		for (unsigned i = 0; i < this->statesCnt; i++)
+//		{
+//			this->states[i] = states[i];
+//		}
+//
+//		if (finalStates != nullptr) {
+//			for (unsigned j = 0; j < this->finalStatesCnt; j++)
+//			{
+//				this->finalStates[j] = finalStates[j];
+//			}
+//		}
+//		else this->finalStates = nullptr;
+//	}
+//	else this->states = nullptr;
+//
+//}
+//DFAutomaton<char>::DFAutomaton(const DFAutomaton<char>& rhs)
+//	:statesCnt(rhs.statesCnt), states(new State[rhs.statesCnt]), entryState(rhs.entryState), finalStatesCnt(rhs.finalStatesCnt),
+//	finalStates(new State[rhs.finalStatesCnt]) {
+//	if (rhs.alphabet != nullptr) {
+//		alphabetSize = rhs.alphabetSize;
+//		alphabet = new char[alphabetSize + 1];
+//		strcpy_s(alphabet, alphabetSize + 1, rhs.alphabet);
+//	}
+//	else {
+//		alphabetSize = 0;
+//		alphabet = nullptr;
+//	}
+//
+//	if (rhs.states != nullptr) {
+//		for (unsigned i = 0; i < this->statesCnt; i++)
+//		{
+//			this->states[i] = rhs.states[i];
+//		}
+//
+//		if (rhs.finalStates != nullptr) {
+//			for (unsigned j = 0; j < this->finalStatesCnt; j++)
+//			{
+//				this->finalStates[j] = rhs.finalStates[j];
+//			}
+//		}
+//		else this->finalStates = nullptr;
+//	}
+//	else this->states = nullptr;
+//}
+//DFAutomaton<char>& DFAutomaton<char>::operator=(const DFAutomaton<char>& rhs) {
+//	if (this != &rhs) {
+//		if (rhs.alphabet != nullptr) {
+//			alphabetSize = rhs.alphabetSize;
+//			alphabet = new char[alphabetSize + 1];
+//			strcpy_s(alphabet, alphabetSize + 1, rhs.alphabet);
+//		}
+//		if (rhs.states != nullptr) {
+//			this->statesCnt = rhs.statesCnt;
+//			for (unsigned i = 0; i < this->statesCnt; i++)
+//			{
+//				this->states[i] = rhs.states[i];
+//			}
+//
+//			if (rhs.finalStates != nullptr) {
+//				this->finalStatesCnt = rhs.finalStatesCnt;
+//				for (unsigned i = 0; i < this->finalStatesCnt; i++)
+//				{
+//					this->finalStates[i] = rhs.finalStates[i];
+//				}
+//			}
+//			else {
+//				this->finalStatesCnt = 0;
+//				this->finalStates = nullptr;
+//			}
+//		}
+//		else {
+//			this->statesCnt = 0;
+//			this->states = nullptr;
+//		}
+//	}
+//
+//	return *this;
+//}
